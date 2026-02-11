fa27e83f46c2bcfb80f6fd1fe4617780000aed03
iharaszti <iharaszti93@gmail.com>
2026-02-11 16:43:51 +0100

    exercise 1.1 - init, add basic components

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,46 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        parts={[
+          { name: part1, exercises: exercises1 },
+          { name: part2, exercises: exercises2 },
+          { name: part3, exercises: exercises3 },
+        ]}
+      />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
+    </div>
+  )
+}
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
+}
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <p key={part.name}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+    </>
+  )
+}
+
+const Total = ({ exercises }) => {
+  const total = exercises.reduce((sum, exercise) => sum + exercise, 0)
+  return <p>Number of exercises {total}</p>
+}
+
+export default App
