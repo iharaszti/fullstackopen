f31cde4cd1fd3bc13a013c5db6584376933202eb
iharaszti <iharaszti93@gmail.com>
2026-02-13 12:17:35 +0100

    Exercise 1.9-1.10 - creating new components

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,24 +1,5 @@
 import { useState } from 'react'
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-
-const Statistics = ({ good, neutral, bad }) => {
-  const all = good + neutral + bad
-  const average = (good - bad) / all || 0
-  const positive = (good / all) * 100 || 0
-
-  return (
-    <>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
-    </>
-  )
-}
-
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -36,5 +17,31 @@ const App = () => {
     </div>
   )
 }
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
+
+  if (all === 0) {
+    return <p>No feedback given</p>
+  }
+
+  const average = (good - bad) / all || 0
+  const positive = (good / all) * 100 || 0
+
+  return (
+    <div>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive} />
+    </div>
+  )
+}
+
+const StatisticLine = ({ text, value }) => <p>{`${text} ${value} ${text === 'positive' ? '%' : ''}`}</p>
 
 export default App
