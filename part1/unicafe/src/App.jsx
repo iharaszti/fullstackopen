3ac3c6ebd931513f208cb07e56e2893cce7f4421
iharaszti <iharaszti93@gmail.com>
2026-02-13 12:20:58 +0100

    add table formatting

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -31,17 +31,24 @@ const Statistics = ({ good, neutral, bad }) => {
   const positive = (good / all) * 100 || 0
 
   return (
-    <div>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
+      </tbody>
+    </table>
   )
 }
 
-const StatisticLine = ({ text, value }) => <p>{`${text} ${value} ${text === 'positive' ? '%' : ''}`}</p>
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{`${value} ${text === 'positive' ? '%' : ''}`}</td>
+  </tr>
+)
 
 export default App
